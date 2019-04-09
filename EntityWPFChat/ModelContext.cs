8b755e2f326dec9140ca657b35ccc7432e3997ff
Martyniuk Roman <hjbfrcgq@gmail.com>
2019-04-09 10:19:52 +0300

    database entity updated so now it works

--- EntityWPFChat/ModelContext.cs
+++ EntityWPFChat/ModelContext.cs
@@ -14,7 +14,7 @@ namespace EntityWPFChat
         // If you wish to target a different database and/or database provider, modify the 'ModelContext' 
         // connection string in the application configuration file.
         public ModelContext()
-            : base("name=ModelContext")
+            : base("name=Somee")
         {
         }
 
